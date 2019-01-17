a1ff9949d51f1c4f9fe08dd0afc967b2407e8755
qtr-ptc-69 <qtr-ptc-69@ptc.local>
2019-01-17 03:54:47 -0800

    cp1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         System.out.println("hello world!");
         System.out.println("another one");
         System.out.println("feature");
@@ -9,5 +10,6 @@ public class Main {
         System.out.println("one text too far");
         System.out.println("master1");
         System.out.println("ot1");
+        System.out.println("feature");
     }
 }
