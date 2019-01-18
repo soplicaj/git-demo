1e2cf060cbdb3e1418b9132368788ddd9ea364bc
jacek <jacek@jacek.pl>
2019-01-18 03:09:49 -0800

    ot1

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("reset to this one");
         System.out.println("one text too far");
         System.out.println("master1");
+        System.out.println("ot1");
     }
 }
